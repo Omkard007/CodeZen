3ca31caad36068ccf133744b89a8b73dfbba0064
SKB <shubhambhilare4899223@gmail.com>
2026-01-17 18:35:11 +0530

    footer changes

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -7,9 +7,9 @@ import { COURSES } from "@/lib/data";
 
 export function Footer() {
   return (
-    <footer className="border-t border-foreground/5 bg-background flex flex-col-reverse md:flex-row py-10 max-w-6xl mx-auto">
-      <div className="container mx-auto px-4 py-8 w-[30%] flex">
-        <div className="pt-8 border-foreground/5 flex flex-col md:flex-row justify-between items-center gap-4">
+    <footer className="border-t border-foreground/5 bg-background flex flex-col-reverse items-center md:flex-row py-10 max-w-6xl mx-auto">
+      <div className="container mx-auto px-4 py-8 md:w-[30%] flex">
+        <div className="pt-8 border-foreground/5 flex flex-col md:flex-row justify-between items-center gap-4 w-full">
           <div className="text-sm text-muted-foreground flex flex-col justify-center items-center w-full">
             <Link href="/" className="inline-block mb-2 group">
               <Logo size="md" />
@@ -19,14 +19,14 @@ export function Footer() {
         </div>
       </div>
       <div className=" w-px bg-black/50"></div>
-      <div className="w-[70%]">
+      <div className="md:w-[70%]">
         {/* <h1 className=" font-bold text-center mb-2">Courses</h1> */}
-        <ul className="grid grid-cols-2 gap-4 justify-items-center">
+        <ul className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 justify-items-center">
           {COURSES.map((course) => (
             <li key={course.title}>
               <Link
                 href={`/courses/${course.id}`}
-                className="text-sm text-muted-foreground hover:text-primary transition-colors"
+                className="text-xs md:text-sm text-muted-foreground hover:text-primary transition-colors"
               >
                 {course.title}
               </Link>
